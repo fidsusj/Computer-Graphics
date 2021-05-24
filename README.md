25ae0dc1b37818454fe840b1afa33c91077f6079
Felix ツ <40273330+fidsusj@users.noreply.github.com>
2021-05-24 12:33:32 +0200

    added exercise 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Algorithm Engineering
+# Computer Graphics
 
-Project to maintain the exercises for the algorithm engineering lecture at University Heidelberg.
+Project to maintain the exercises for the computer graphics lecture at University Heidelberg.
+
+For all solutions we build upon the published sample solution from the last exercise. 
--- exercise_2/src/Raytracer/Scenes/PointLight.cpp
+++ exercise_2/src/Raytracer/Scenes/PointLight.cpp
@@ -11,9 +11,47 @@ PointLight::PointLight(const float3 &position, const float3 &intensity)
 
 float3 PointLight::ComputeDirectContribution(const Intersection &intersection, const Scene &scene)
 {
-  // TODO: Implement the calculation of the diffuse and specular lighting according to the
-  // Phong light modelat the point determined by the intersection calculation.
-  // Also respect that the intensity of the light will decrease with increasing distance to the light source,
-  // or by positioning them behind other objects (shadowing).
-  return float3(0, 0, 0);
+	// TODO: Implement the calculation of the diffuse and specular lighting according to the
+	// Phong light model at the point determined by the intersection calculation.
+	// Also respect that the intensity of the light will decrease with increasing distance to the light source,
+	// or by positioning them behind other objects (shadowing).
+	float dist2Light = length(position - intersection.position);
+	float3 Il = intensity * (1 / powf(dist2Light, 2));
+	float3 N = intersection.normal;
+	float3 L = normalize(position - intersection.position);
+
+	// Ambient term
+	float3 I = intersection.material->GetAmbient() * Il;
+
+	// Shadow ray
+	Ray ray = Ray(intersection.position + 0.001f * intersection.normal, L);  // epsilon = 0.001f
+	for (Raytracer::Scenes::IPrimitive* object: scene.GetObjects())
+	{
+		RayHit objectHit;
+		object->HitTest(ray, objectHit);
+		float t = objectHit.GetDistance();
+		if (t > 0 && t < dist2Light)
+		{
+			return I;
+		}
+	}
+
+	// Diffuse term
+	float NdotL = dot(N, L);
+	if (NdotL > 0)
+	{
+		I += intersection.material->GetDiffuse() * Il * NdotL;
+		
+		// Specular term
+		float3 R = normalize(2.0f * N * dot(N, L) - L);
+		float3 V = intersection.viewDirection;
+		float RdotV = dot(R, V);
+		if (RdotV > 0)
+		{
+			I += intersection.material->GetSpecular() * Il * powf(RdotV, intersection.material->GetShininess());
+		}
+	}
+
+	// Emissive term is 0
+	return I;
 }
